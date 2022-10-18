388c78652306e9bb56b7efa55915bed45c63bdfe
Eltonszaftc <eltonsouzafatecitu@gmail.com>
2022-10-18 14:42:29 -0300

    seguindo o tutorial, porém com falha

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
@@ -14,5 +16,13 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+}
 
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+}
+selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
 }
