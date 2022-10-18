b414ea28303dbe0c3693ba4f89c15cf10a93c61f
Eltonszaftc <eltonsouzafatecitu@gmail.com>
2022-10-18 14:51:48 -0300

    tentando atualizar

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,21 +9,16 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-}
 
-export class HeroesComponent implements OnInit {
-
-  heroes = HEROES;
-}
-selectedHero?: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
